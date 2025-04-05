cf914d7595b53355cb80ba5c4d366af0771950ae
Ubuntu <ubuntu@ip-172-31-13-118.ap-south-1.compute.internal>
2025-04-05 05:14:05 +0000

    signup updated in server

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,40 +1,214 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-const referal=document.getElementById("refer")
-const referalCode=document.getElementById('referalCode')
-const referBtn=document.getElementById('referBtn')
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-    referal.classList.remove("hide")
-});
+      const sign_in_btn = document.querySelector("#sign-in-btn");
+      const sign_up_btn = document.querySelector("#sign-up-btn");
+      const container = document.querySelector(".container");
+      const login=document.getElementById("login")
+      const signup=document.getElementById("signup")
+//signup graphics
+      sign_up_btn.addEventListener("click", () => {
+        container.classList.add("sign-up-mode");
+      });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-    referal.classList.add("hide")
-});
-referBtn.addEventListener('click',()=>
+      sign_in_btn.addEventListener("click", () => {
+        container.classList.remove("sign-up-mode");
+      });
+//password toggle
+      function togglePassword(id) {
+        const passwordField = document.getElementById(id);
+        const toggleIcon = passwordField.nextElementSibling;
+        if (passwordField.type === "password") {
+          passwordField.type = "text";
+          toggleIcon.classList.remove("bi-eye");
+          toggleIcon.classList.add("bi-eye-slash");
+        } else {
+          passwordField.type = "password";
+          toggleIcon.classList.remove("bi-eye-slash");
+          toggleIcon.classList.add("bi-eye");
+        }
+      }
+
+// login actions
+login.addEventListener("click",()=>
 {
-  referalCode.classList.remove('hide')
-})
+    const email=document.getElementById("signin-email").value;
+    const password=document.getElementById("signin-password").value;
+       const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-function togglePassword() {
-    const passwordField = document.getElementById("password");
-    const confirmPasswordField = document.getElementById("confirmPassword");
+    if(email === ''||password ==='')
+        {
+            Swal.fire({
+                title: 'All fields are required!',
+                icon: 'error',
+                toast: true,
+                position: 'top-start',
+                showConfirmButton: false,
+                timer: 3000
+            });
+          
+        }
+          else if (!regex.test(email)) {
+            Swal.fire({
+                title: 'Invalid Email!',
+                icon: 'error',
+                toast: true,
+                position: 'top-start',
+                showConfirmButton: false,
+                timer: 3000
+            });
+            return;
+        }else{
 
-    const icon = document.getElementById("togglePassword");
-
-    // If password is visible, hide it, and vice versa
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        confirmPasswordField.type = "text";
-        icon.classList.remove("fa-eye");
-        icon.classList.add("fa-eye-slash");
-    } else {
-        passwordField.type = "password";
-        confirmPasswordField.type = "password";
-        icon.classList.remove("fa-eye-slash");
-        icon.classList.add("fa-eye");
-    }
+        $.ajax({
+            url: "/login",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ fullName, mobile }),
+            success: function (response) {
+                Swal.fire({
+                    title: 'Login success',
+                    icon: 'success',
+                    toast: true,
+                    position: 'top-center',
+                    showConfirmButton: false,
+                    timer: 3000
+                }).then(()=>
+                {
+                    window.href="/"
+                })
+            },
+            error: function (err) {
+                if (err.responseJSON && err.responseJSON.message) {
+                    Swal.fire({
+                        title: err.responseJSON.message,
+                        icon: 'error',
+                        toast: true,
+                        position: 'top-start',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                } else {
+                    Swal.fire({
+                        title: 'Something went wrong!',
+                        icon: 'error',
+                        toast: true,
+                        position: 'top-center',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                }
+            }
+            
+          });
 }
+       
+})
+//signup actions
+signup.addEventListener('click',()=>
+{
+    const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const phone = document.getElementById("phone").value;
+        const password = document.getElementById("signup-password").value;
+        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const phonePattern = /^(\+?[0-9]{1,4})?[-\s]?[0-9]{10}$/;
+         const regexpass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+                
+        if (name === '' || email === '' || phone === '' || password === '') {
+            Swal.fire({
+                title: 'All fields are required!',
+                icon: 'error',
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+            });
+            
+               }
+    
+        // Email validation
+             else  if (!regex.test(email)) {
+            Swal.fire({
+                title: 'Invalid Email!',
+                icon: 'error',
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+            });
+           
+        }
+    
+          else if (!phonePattern.test(phone)) {
+            Swal.fire({
+                title: 'invalid phoneNumber!',
+                icon: 'error',
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+            });
+                  }
+    
+        // Password validation (strong password check)
+              else if (!regexpass.test(password)) {
+            Swal.fire({
+                title: 'Choose a Strong Password!(Example@123)',
+                icon: 'error',
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+            });
+            return false;
+        }
+        else
+          {
+    
+        $.ajax({
+            url: "/signup",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ name,email,phone,password }),
+            success: function (response) {
+                Swal.fire({
+                    title: 'Otp sent',
+                    icon: 'success',
+                    toast: true,
+                    position: 'top-center',
+                    showConfirmButton: false,
+                    timer: 3000
+                }).then(()=>
+                {
+                    window.href="/verify-otp"
+                })
+            },
+            error: function (err) {
+                if (err.responseJSON && err.responseJSON.message) {
+                    Swal.fire({
+                        title: err.responseJSON.message,
+                        icon: 'error',
+                        toast: true,
+                        position: 'top-start',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                } else {
+                    Swal.fire({
+                        title: 'Something went wrong!',
+                        icon: 'error',
+                        toast: true,
+                        position: 'top-center',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                }
+            }
+            
+          });
+}
+    
+})
+      
+    
+   
